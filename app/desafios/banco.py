a4a882f91fda9d70c7c54e85fe572b220289c87a
DaviIshikawa <davicishikawa@outlook.com>
2026-06-15 21:44:29 -0300

    aula 8 django

--- app/desafios/banco.py
+++ app/desafios/banco.py
@@ -1,20 +1,16 @@
 #cliente
-
 class Cliente:
     def __init__(self,nome,cpf):
-        self.nome = nome
-        self.cpf = cpf
-
-    def __str__(self):
-        return f"Cadastro realizado:\n nome:{self.nome} - CPF:{self.cpf}"
+       self.nome = nome
+       self.cpf = cpf 
     
-cliente1 = Cliente("Maria","108.545.434-35")
-print(cliente1)
+    def __str__(self):
+        return f"Cadastro realizado:\n nome: {self.nome} cpf: {self.cpf}"
+
 
 #conta
-
 class Conta:
-    def __init__(self,numero,cliente,saldo=0):
+    def __init__(self,numero,cliente):
         self.numero = numero
         self.cliente = cliente
         self.saldo = 0
@@ -22,22 +18,23 @@ class Conta:
     def depositar(self,valor):
         if valor > 0:
             self.saldo += valor
-            print(f"Depósito de R$ {valor:2f} realizado")
+            print(f"Deposito de R$ {valor:.2f} realizado")
         else:
-            print(f"Valor inválido")
+            print("Valor inválido")
+    
     def sacar(self,valor):
         if valor <= self.saldo:
             self.saldo -= valor
-            print(f"Saque d R$ {valor:2f} realizado")
+            print(f"Saque de R$ {valor:.2f} realizado")
         else:
-            print("Saldo insuficiente")
+            print("saldo insuficiente")
+
     def transferir(self,destino,valor):
         if valor <= self.saldo:
-            self.saldo
             self.saldo -= valor
             print("Transferência realizada")
         else:
-            print("Saldo insuficiente")
+            print("saldo insuficiente")
 
     def exibir_saldo(self):
         print(f"Saldo atual R$ {self.saldo}")
@@ -46,26 +43,56 @@ class Conta:
         return(
             f"Conta:{self.numero}\n"
             f"Titular:{self.cliente.nome}\n"
-            f"Saldo{self.saldo}"
+            f"Saldo: R${self.saldo}"
         )
     
 #Herança conta corrente
-class ContaCorrrente(Conta)
-    def __init__(self, numero, cliente, limite):
+class ContaCorrente(Conta):
+    def __init__(self,numero,cliente,limite):
         super().__init__(numero,cliente)
         self.limite = limite
-        
+
     def sacar(self, valor):
         if valor <=(self.saldo+self.limite):
             self.saldo -= valor
             print(f"Saque de R$ {valor} realizado")
         else:
-            print("Saldo insuficiente")
+            print("saldo insuficiente")
+    
     def __str__(self):
         return(
-            f"Conta {self.numero}zn"
-            f"Titular {self.Cliente.nome}\n"
-            f"Saldo R$ {self.saldo}\n"
-            f"Limite R$ {self.limite}"
-                )
+            f"Conta:{self.numero}\n"
+            f"Titular:{self.cliente.nome}\n"
+            f"Saldo: R${self.saldo}\n"
+            f"Limite: R$ {self.limite}"
+        )
 #Herança conta poupança
+class ContaPoupanca(Conta):
+    def __str__(self):
+        return(
+            f"Conta poupança:\n "
+            f"Conta:{self.numero}\n"
+            f"Titular:{self.cliente.nome}\n"
+            f"Saldo: R${self.saldo}\n"  
+        )
+
+#Programa principal
+
+cliente1 = Cliente("Maria","108.545.454-35")
+cliente2 = Cliente("Carlos","106.595.333-85")
+
+conta1 = ContaCorrente(1001,cliente1,500)
+conta2 = ContaPoupanca(2001,cliente2)
+
+print("\n====Deposito=====")
+conta1.depositar(1200)
+conta2.depositar(4500)
+print("\n====Saque=====")
+conta1.sacar(250)
+
+print("\n====Transferência=====")
+conta1.transferir(conta2,200)
+
+print("\n====Saldos=====")
+conta1.exibir_saldo()
+conta2.exibir_saldo()
